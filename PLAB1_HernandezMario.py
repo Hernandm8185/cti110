50f225079b3e779bfb9a38435f9fe12f558ba6e3
Hernandm8185 <hernandm8185@student.faytechcc.edu>
2025-03-30 16:10:40 -0400

    Add files via upload
    First Submission

---
+++ PLAB1_HernandezMario.py
@@ -0,0 +1,9 @@
+# Mario Hernandez
+# March 30th, 2025
+# PLAB1
+
+firstName = input('Enter your first name: ')
+print ()
+lastName= input('Enter your last name: ')
+print ()
+print ('Hello, '+ firstName + ' ' + lastName+ '! Welcome to CTI-110')
